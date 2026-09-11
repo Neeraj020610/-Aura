4c850af211dd573545f6af2ed0c5a84f26d1145c
unknown <Neerajkumarbamanawat@gmail.com>
2026-09-11 20:27:11 +0530

    fix: Add standard gradlew and explicit imports

--- android_client/app/src/main/java/com/aura/client/AuraService.kt
+++ android_client/app/src/main/java/com/aura/client/AuraService.kt
@@ -6,6 +6,7 @@ import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.net.Uri
 import android.os.BatteryManager
 import android.os.Build
